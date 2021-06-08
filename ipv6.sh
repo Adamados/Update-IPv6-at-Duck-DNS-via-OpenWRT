13173c5b75ac7734bd628573208e8ce3afb87845
Adamados <58473441+Adamados@users.noreply.github.com>
2021-06-08 13:57:43 +0300

    Create ipv6.sh

---
+++ ipv6.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+#https://forum.openwrt.org/t/get-ipv6-address-of-server-for-ddns-update/71425/31
+# */5 * * * * /root/getipv6.sh -d "br-lan" -i "lan" >/dev/null 2>&1
+
+MacAddr=aa:aa:aa:aa:aa:aa
+TrafficRuleName=RULE.NAME
+
+printf "\n"
+printf "Getting your IPv6 address... \n"
+getIPv6=$(ip -6 neigh | grep "$MacAddr" | grep -v "fe80" | grep -v "fdd9" | cut -d" " -f1)
+
+if [ "$getIPv6" = "" ]
+then
+    printf "Failed to get IP."
+    exit 0
+fi
+
+printf "Your current IPv6: {$getIPv6} \n"
+
+changed=0
+index=0
+LFRuleName=$(uci get firewall.@rule[$index].name 2> /dev/null)
+
+while [ "$LFRuleName" != "" ]
+do
+
+    if [ "$LFRuleName" == "$TrafficRuleName" ]
+    then
+        dest_ip=$(uci get firewall.@rule[$index].dest_ip 2> /dev/null)
+        printf "Your stored IPv6:  {$dest_ip} \n"
+
+        if [ "$dest_ip" != "$getIPv6" ]
+        then
+            printf "The IP has changed! \n"
+            printf "Updating...\n"
+            changed=1
+			printf "Answear from DuckDNS: "
+			curl --silent "https://www.duckdns.org/update?domains=indigoha&token=32e50cbd-b600-46e3-abff-13f2890c25a8&ipv6=${getIPv6}"
+			uci set firewall.@rule[$index].dest_ip=$getIPv6
+            uci commit firewall
+        else
+            printf "IP is the same, no changes made.\n\n"
+        fi
+
+        break 2
+	fi
+	
+    index=$(expr $index + 1)
+    LFRuleName=$(uci get firewall.@rule[$index].name 2> /dev/null)
+
+done
+
+if [ "$LFRuleName" = "" ]
+then
+	printf "\n"
+	printf "Failed to find Rule name \n\n"
+fi
+
+if [ $changed -eq 1 ] 
+then
+    printf "\nRestarting firewall... \n"
+    /etc/init.d/firewall reload 2> /dev/null
+    printf "All up to date. \n\n"
+fi
+
+exit 0
